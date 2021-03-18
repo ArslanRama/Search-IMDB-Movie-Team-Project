75d7830b82c75f2fa44eebf88bcaf4163ef7a645
ArslanRama <arslanrama84@gmail.com>
2021-03-18 13:39:05 +0100

    Fetching API is created, only mapping is missing.

---
+++ src/components/Movies.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// here we need the placeholder image which is the poster in API
+// Create a variable with the poster like const ... = "poster link"
+// Create a function with paramater and pass it in return to show the info in browser
+
+function Movies() {
+    return (
+        <div>
+            <h2>Movies</h2>
+        </div>
+    )
+}
+
+export default Movies
