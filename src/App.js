560544c6dc03f19cc633b9604cd6eb155656fc35
ArslanRama <arslanrama84@gmail.com>
2021-03-23 11:55:00 +0100

    Context APi REMOVED

--- src/App.js
+++ src/App.js
@@ -1,27 +1,108 @@
-import React from 'react'
-//* Children Components */
-// import Header from "./components/Movies";
-import Movies from "./components/Movie"
-// import Search from "./context/Search";
-import MovieList from "./context/MovieList"
+import React, { useReducer, useEffect } from "react";
+import "./App.css";
 
-//* Navigaton Components */
-import NavBar from "./Navigation/NavBar";
-import MyProvider from './context/MovieContext';
-// import MovieList from './components/MovieList';
+/* Components */
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import Search from "./components/Search";
 
-function App() {
-   
+/* API */
+const API_KEY = "d1ce076e";
 
-    return (
-        <MyProvider>
-        <div>
-            <NavBar />
-            <MovieList />
-            <Movies />
-        </div>
-        </MyProvider>
-    )
-}
+const MOVIE_API_URL = `https://www.omdbapi.com/?s=man&apikey=${API_KEY}`;
 
-export default App
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+};
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+        .then(response => response.json())
+        .then(jsonResponse => {
+    
+        dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search
+      });
+    });
+  }, []);
+
+  const search = searchValue => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+    
+/* Fetching the data */
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=${API_KEY}`)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      if (jsonResponse.Response === "True") {
+        dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search
+        });
+      } else {
+        dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.Error
+        });
+      }
+    });
+  };
+
+  const { movies, errorMessage, loading } = state;
+
+  return (
+    <div className="App">
+      <div className="Nav">
+        <Header text="Search IMDB MOVIES with HOOK" />
+        <Search search={search} />
+      </div>
+      <p className="App-intro"></p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span className="loader"></span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
